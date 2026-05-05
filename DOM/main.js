9d11a2a678f963364681361c16b5bc301c14c798
magda chikhradz <magda292005@gmail.com>
2026-05-05 17:40:40 +0400

    objects

---
+++ DOM/main.js
@@ -0,0 +1,60 @@
+// objects
+
+const colors = ["red", "green", "blue"];
+
+// const user = {}; //how objects are created
+
+const user = {
+  userName: "athena",
+  age: 23,
+  isActive: true,
+};
+
+console.log(user.age);
+
+const colorsTwo = colors;
+
+colorsTwo[0] = "purple";
+
+console.log(colors);
+console.log(colorsTwo);
+
+const admin = user;
+
+admin.post = true;
+
+//
+//make a copy - spread op.
+
+const obj = {
+  name: "magda",
+};
+
+const adminOne = {
+  ...user,
+  ...obj,
+};
+
+adminOne.post = true;
+
+//
+
+const newArr = [...colors];
+
+newArr.push("yellow");
+console.log(newArr);
+
+//also named reference types
+//მუტაციური- განიცდის ორიგინალი ცვლილებას
+
+//
+
+// value types - mnishvnelobebzea damokidebuli
+//არა მუტაციური- არ განიცდის ორიგინალი ცვლილებას
+const number = 25;
+const string = "text";
+
+const new1 = string.toUpperCase();
+console.log(new1);
+
+
