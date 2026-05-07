cd1375dd4ebab57a8d0d4c7e2a540bc7faf497d7
Magda Chikhradze <magda292005@gmail.com>
2026-05-07 18:04:08 +0400

    add validation

--- DOM_2/script-2.js
+++ DOM_2/script-2.js
@@ -18,3 +18,33 @@ form.addEventListener("submit", (e) => {
 });
 
 //formdata connects to the name attribute
+
+// const form  = document.getElementById('register_form')
+
+// form.addEventListener('submit', (e) =>{
+//     e.preventDefault()
+
+//     const data = new FormData(form)
+
+//     const name = data.get('username')
+
+//     if(name.length < 2){
+//       return  errorHandler('name is invalid')
+//     }
+
+//     const user = {
+//         name: name,
+//         lastname: data.get('lastname'),
+//         email: data.get('email'),
+//         password: data.get('password'),
+//         gender: data.get('gender'),
+//         terms: data.get('terms')
+//     }
+
+//     console.log(user)
+
+// })
+
+// function errorHandler(errorText) {
+//   error_message.textContent = errorText;
+// }
